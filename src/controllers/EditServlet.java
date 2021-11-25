56ad84837d13f0db098a6854123b0bc6ab8833fa
ykosh <ykosh@DESKTOP-FU152O7>
2021-11-26 00:12:22 +0900

    UpdateServlet.javaを作成

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -40,6 +40,8 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("task", t);
         request.setAttribute("_token", request.getSession().getId());
 
+        request.getSession().setAttribute("task_id", t.getId());
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
     }
