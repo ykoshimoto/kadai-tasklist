5611980896f73814b4316415c6549a8e4875b5d3
ykosh <ykosh@DESKTOP-FU152O7>
2021-11-26 21:09:31 +0900

    ページネーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -34,13 +34,29 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+        //ページネーション
+        //開くページ数を取得
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch (NumberFormatException e) {}
+
+        //最大件数と開始位置を指定、データベースから取得
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+                              .setFirstResult(15 * (page - 1))
+                              .setMaxResults(15)
+                              .getResultList();
+        //全件数を取得
+        long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
+                                    .getSingleResult();
 
         em.close();
 
         request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count);
+        request.setAttribute("page", page);
 
-        //flushメッセージがセッションスコープにある場合はリクエストスコープに登録し、削除
+        //flushメッセージがセッションスコープにある場合はリクエストスコープに登録し、セッションスコープから削除
         if(request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
